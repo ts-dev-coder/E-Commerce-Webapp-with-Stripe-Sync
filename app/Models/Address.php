d3f7865d3a56c068621e55b77d2c016259230c5b
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-07-16 19:25:42 +0900

    Add Address model class

---
+++ app/Models/Address.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'recipient_name',
+        'postal_code',
+        'prefecture',
+        'city',
+        'street',
+        'building',
+        'phone_number',
+        'is_default',
+        'address_type',
+        'user_id'
+    ];
+
+    # TODO: Add relation definition
+}
