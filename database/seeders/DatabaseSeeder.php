bfd0692e29bcfa4c9a3d4709b395fd4c84c0c707
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-07-17 13:29:48 +0900

    Comment out seeder code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+// use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-         $this->call([
-            ProductSeeder::class,
-        ]);
+        //  $this->call([
+        //     ProductSeeder::class,
+        // ]);
     }
 }
