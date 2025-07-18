62aa96271fe1ab078930ed083061feb35d4cedb6
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-07-18 21:26:04 +0900

    Home route updated to use HomeController instead of inline function

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get('/', HomeController::class)->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
