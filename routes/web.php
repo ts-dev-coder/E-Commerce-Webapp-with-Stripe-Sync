75997f5123a35d3549afeda8eb8ed4466e8ad3cc
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-07-22 15:39:50 +0900

    Add product detail page route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\ProductDetailController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', HomeController::class)->name('home');
+ROute::get('/products/{id}', ProductDetailController::class)->name('product-detail');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
