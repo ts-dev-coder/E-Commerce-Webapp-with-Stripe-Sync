59f99a8ae3f651cf07e639cb795033cf1150834f
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-07-23 11:59:39 +0900

    Add OrderCompleteController class

---
+++ app/Models/OrderCompleteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class OrderCompleteController extends Model
+{
+    public function __invoke()
+    {
+        $user = Auth::user();
+
+        // TODO: If the user is logged in, retrieve the count of cart items
+        //       from the database
+        $cartItemCount = 0;
+
+        // TODO: Change response data to thanks page component
+        return response()->json([
+            'message' => 'hello world',
+            'cartItemCount' => $cartItemCount
+        ]);
+    }
+}
