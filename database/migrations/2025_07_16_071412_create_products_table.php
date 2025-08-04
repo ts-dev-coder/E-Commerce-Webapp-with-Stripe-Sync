2f1b19fbc730258942bbbd6aaf8fd1ef1490ce5b
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-08-04 15:00:50 +0900

    Add new column: available_from available_until

--- database/migrations/2025_07_16_071412_create_products_table.php
+++ database/migrations/2025_07_16_071412_create_products_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            
             $table->string('name', 100);
             $table->text('description')->nullable();
             $table->unsignedInteger('price');
             $table->unsignedInteger('stock');
+
             $table->boolean('is_published')->default(false);
             $table->dateTime('published_at')->nullable();
+
+            $table->dateTime('available_from')->nullable(); // 商品販売日
+            $table->dateTime('available_until')->nullable(); // 商品販売終了日
             
             $table->timestamps();
         });
