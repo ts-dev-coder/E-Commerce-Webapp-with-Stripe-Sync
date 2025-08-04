e5f53db4aed0f02b54f6e406eb56689858c18456
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-08-04 21:01:15 +0900

    Add available_from, available_until propaty to tha Product Factory class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        //  $this->call([
-        //     ProductSeeder::class,
-        // ]);
+         $this->call([
+            ProductSeeder::class,
+        ]);
     }
 }
