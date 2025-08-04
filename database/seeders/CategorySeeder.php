26780d0ec8f1feeff13365a38f9f021304e4217a
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-08-04 17:53:12 +0900

    Add CategorySeeder file

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            ['name' => 'Electronics', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Fashion', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Books', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Home & Kitchen', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Sports', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Music', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Game', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+}
