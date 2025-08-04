5e19bbd667061245d4c0616862a30671d9e8e786
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-08-04 21:08:13 +0900

    Remove commented User factory call and ensure UserSeeder is included in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-// use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
          $this->call([
+            UserSeeder::class,
             ProductSeeder::class,
         ]);
     }
