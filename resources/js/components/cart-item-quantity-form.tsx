dcf14d804a7a4c47be4779de3b61b9053f7caa78
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-08-12 13:01:43 +0900

    Add CartItemQuantityForm component prototype UI

---
+++ resources/js/components/cart-item-quantity-form.tsx
@@ -0,0 +1,22 @@
+import { Button } from './ui/button';
+
+export function CartItemQuantityForm() {
+    return (
+        <form>
+            <div className="flex items-center gap-x-2">
+                <Button type="submit" size={'sm'}>
+                    -
+                </Button>
+                <div className="rounded-lg border border-slate-400/50 px-4 py-1">
+                    <span>10</span>
+                </div>
+                <Button type="submit" size={'sm'}>
+                    +
+                </Button>
+            </div>
+            <div>
+                <span className="text-sm text-red-500">error message</span>
+            </div>
+        </form>
+    );
+}
