ca521ff33d0892289fbeb5931099309590709b6a
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-08-12 16:05:44 +0900

    Add patch method

--- resources/js/components/cart-item-quantity-form.tsx
+++ resources/js/components/cart-item-quantity-form.tsx
@@ -1,4 +1,5 @@
 import { useForm } from '@inertiajs/react';
+import { FormEventHandler } from 'react';
 
 import { Button } from './ui/button';
 
@@ -14,12 +15,13 @@ type UpdateCartItemQuantityForm = {
     quantity: number;
 };
 
-export function CartItemQuantityForm({ productId, maxQuantity, quantity }: Props) {
-    const { data, setData } = useForm<UpdateCartItemQuantityForm>({
+export function CartItemQuantityForm({ cartItemId, productId, maxQuantity, quantity }: Props) {
+    const { data, setData, patch, processing } = useForm<UpdateCartItemQuantityForm>({
         product_id: productId,
         quantity: quantity,
     });
 
+    // TODO: 2から１の変更時にform eventが発火しない為、下記の2つの状態を変更する
     const availableMinus = data.quantity === 1;
     const availablePlus = data.quantity >= maxQuantity;
 
@@ -38,16 +40,26 @@ export function CartItemQuantityForm({ productId, maxQuantity, quantity }: Props
         setData('quantity', updatedQuantity);
     };
 
+    const handleUpdateQuantity: FormEventHandler = (e) => {
+        e.preventDefault();
+
+        patch(route('cart.updateQuantity', { item: cartItemId }), {
+            onSuccess: () => {
+                console.log('success');
+            },
+        });
+    };
+
     return (
-        <form>
+        <form onSubmit={handleUpdateQuantity}>
             <div className="flex items-center gap-x-2">
-                <Button type="button" size={'sm'} onClick={() => changeQuantity(-1)} disabled={availableMinus}>
+                <Button type="submit" size={'sm'} onClick={() => changeQuantity(-1)} disabled={availableMinus || processing}>
                     -
                 </Button>
                 <div className="w-14 rounded-lg border border-slate-400/50 px-4 py-1 text-center">
                     <span className="text-sm font-semibold">{data.quantity}</span>
                 </div>
-                <Button type="button" size={'sm'} onClick={() => changeQuantity(1)} disabled={availablePlus}>
+                <Button type="submit" size={'sm'} onClick={() => changeQuantity(1)} disabled={availablePlus || processing}>
                     +
                 </Button>
             </div>
