536202afe08ab3de4c50ef7efb23765fb6b4a1d2
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-08-13 15:10:39 +0900

    Add addresses table

---
+++ database/migrations/2025_08_13_140520_create_addresses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->string('recipient_name', 50); // お届け先氏名
+
+            $table->string('postal_code', 16);     // 例: 1000001, 100-0001 など（バリデーションはアプリ側で）
+            $table->string('prefecture', 50);      // 都道府県
+            $table->string('city', 100);           // 市区町村（区を含む）
+            $table->string('street', 150);         // 町名・丁目・番地・号
+            $table->string('building', 150)->nullable(); // 建物名・部屋番号
+
+            $table->string('phone_number', 10); // 電話番号
+
+            $table->boolean('is_default')->default(false); // デフォルトとするかどうかの判定フラグ
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
