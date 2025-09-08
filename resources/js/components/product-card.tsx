5233fdfc6680b3af28d9b90d407ac2ef122fbf91
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-09-08 17:39:44 +0900

    Fix product card UI

--- resources/js/components/product-card.tsx
+++ resources/js/components/product-card.tsx
@@ -1,31 +1,22 @@
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Product } from '@/types';
-import { Link } from '@inertiajs/react';
 
 export const ProductCard = ({ product }: { product: Product }) => {
     return (
-        <Card className="flex min-h-full w-full max-w-xs flex-col justify-between shadow-lg">
-            <CardHeader>
-                <CardTitle className="text-lg font-semibold">{product.name}</CardTitle>
-            </CardHeader>
-            <CardContent className="flex flex-1 flex-col">
-                {/* TODO: Add product image */}
-                <img src={'https://placehold.co/400x300?text=No+Image'} alt={product.name} className="mb-4 h-40 w-full rounded object-cover" />
-                <p className="mb-2 text-sm text-muted-foreground">{product.description}</p>
-                <div className="flex-1" />
-                <div className="mb-2 flex items-baseline justify-center">
-                    <span className="mr-1 text-base text-primary">¥</span>
-                    <span className="text-xl font-bold">{product.price}</span>
+        <div className="flex h-52 w-40 flex-col space-y-2 pr-4">
+            {/* 商品画像 */}
+            <div className="size-full">
+                <img src={'https://placehold.co/400x300?text=No+Image'} alt={product.name} className="size-full object-cover" />
+            </div>
+
+            <div className="flex h-full flex-col items-center justify-between pb-3">
+                {/* 商品名 */}
+                <span className="line-clamp-2 text-base">{product.name}</span>
+
+                {/* 商品価格 */}
+                <div className="w-full text-end">
+                    <span className="font-semibold text-red-600">{product.price.toLocaleString('ja-JP')}円</span>
                 </div>
-            </CardContent>
-            <CardFooter>
-                <Link href={`/products/${product.id}`} className="w-full">
-                    <Button className="w-full" variant="outline">
-                        詳細を見る
-                    </Button>
-                </Link>
-            </CardFooter>
-        </Card>
+            </div>
+        </div>
     );
 };
