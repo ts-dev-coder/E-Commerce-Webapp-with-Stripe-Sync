7c9d1d7cf7362c5c1740379976260a69fc3779af
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-09-25 16:07:19 +0900

    Add toggleDefault method to AddressService class

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -8,11 +8,17 @@ use App\Models\Address;
 
 use App\Repositories\AddressRepository;
 
+use App\Services\AddressService;
+
+use Illuminate\Support\Facades\Auth;
 
 class AddressController extends Controller
 {
-    public function store(StoreAddressRequest $request, AddressRepository $addressRepository) {
-        // TODO:デフォルトで設定しているものがある場合はFalseに変更する
+    public function store(StoreAddressRequest $request, AddressRepository $addressRepository, AddressService $addressService) {
+        
+        if($request->validated('is_default')) {
+            $addressService->toggleDefault(Auth::user());
+        }
 
         $validatedData = $request->validated();
 
