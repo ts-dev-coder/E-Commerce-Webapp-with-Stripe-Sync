5f75c2bc83ead3b1166d92bcc8bf959c7ce2493c
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-09-26 13:19:19 +0900

    Change reponse: json to redirect to chekcout.index route

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Auth;
 class AddressController extends Controller
 {
     public function store(StoreAddressRequest $request, AddressRepository $addressRepository, AddressService $addressService) {
-        
+
         if($request->validated('is_default')) {
             $addressService->toggleDefault(Auth::user());
         }
@@ -26,8 +26,12 @@ class AddressController extends Controller
 
         $addressRepository->createAddresss($address);
 
-        return response()->json([
-            'message' => 'successfully.',
-        ]);
+        /**
+         * 現状は、チェックアウト画面からのみ住所登録を行える設計なので
+         * chekcout.indexにリダイレクトさせる。
+         * 今後マイページなどで住所登録を行う場合は、以下のように変更する可能性あり
+         * redirect()->back();
+         */
+        return redirect()->route('checkout.index');
     }
 }
