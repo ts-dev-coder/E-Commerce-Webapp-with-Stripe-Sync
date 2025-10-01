d9b8ae2b8f697dacb3b273780ba6a45db14a5343
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-10-01 13:27:07 +0900

    Add banner carousel component

--- resources/js/layouts/app/app-header-layout.tsx
+++ resources/js/layouts/app/app-header-layout.tsx
@@ -3,12 +3,13 @@ import type { PropsWithChildren } from 'react';
 import { AppContent } from '@/components/app-content';
 import { AppHeader } from '@/components/app-header';
 import { AppShell } from '@/components/app-shell';
+import { BannerCarousel } from '@/components/banner-carousel';
 
 export default function AppHeaderLayout({ children, cartItemCount }: PropsWithChildren<{ cartItemCount: number }>) {
     return (
         <AppShell>
             <AppHeader cartItemCount={cartItemCount} />
-            <div>カルーセルバナー</div>
+            <BannerCarousel />
             <AppContent>{children}</AppContent>
         </AppShell>
     );
