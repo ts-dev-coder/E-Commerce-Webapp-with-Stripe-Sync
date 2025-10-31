300dcf8430be6ca0ec4d82676a9f8584ba6e841a
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-10-31 17:17:44 +0900

    Add Dashboard page prototype

---
+++ resources/js/pages/admin/dashboard.tsx
@@ -0,0 +1,9 @@
+import AdminLayout from '@/layouts/admin-layout';
+
+export default function Dashboard() {
+    return (
+        <AdminLayout>
+            <div className="flex size-full items-center justify-center text-5xl font-bold">Dashboard page</div>
+        </AdminLayout>
+    );
+}
