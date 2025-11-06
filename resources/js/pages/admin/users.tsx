44b5731a348ecbd0384fa2ae35b0c14a88b1bfcc
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-06 12:55:08 +0900

    Add users table UI in the users page

--- resources/js/pages/admin/users.tsx
+++ resources/js/pages/admin/users.tsx
@@ -1,9 +1,42 @@
 import AdminLayout from '@/layouts/admin-layout';
 
-export default function Users() {
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+
+import { User } from '@/types';
+
+type Props = {
+    users: User[];
+};
+
+const userMetaData = ['name', 'email'];
+
+export default function Users({ users }: Props) {
     return (
         <AdminLayout>
-            <div className="flex size-full items-center justify-center text-5xl font-bold">Users page</div>
+            <div className="flex size-full">
+                <Table>
+                    <TableCaption>A list of user.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            {userMetaData.map((metaData) => (
+                                <TableHead>{metaData}</TableHead>
+                            ))}
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {users.map((user, index) => {
+                            const style = index % 2 !== 0 ? 'bg-sky-200/60' : '';
+
+                            return (
+                                <TableRow key={user.id} className={`hover:cursor-pointer hover:bg-sky-300 ${style}`}>
+                                    <TableCell>{user.name}</TableCell>
+                                    <TableCell>{user.email}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </div>
         </AdminLayout>
     );
 }
