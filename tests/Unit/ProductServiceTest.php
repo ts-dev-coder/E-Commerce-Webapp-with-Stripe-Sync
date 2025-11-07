908678963f3d863eb2a38cfd6e89f8a49c5c167b
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-07 21:09:04 +0900

    test code : Add test_product_can_be_updated method

--- tests/Unit/ProductServiceTest.php
+++ tests/Unit/ProductServiceTest.php
@@ -36,4 +36,43 @@ class ProductServiceTest extends TestCase
         // 配列の中身がProduct instanceであること
         $this->assertInstanceOf(Product::class, $products->first());
     }
+
+    public function test_product_can_be_updated()
+    {
+        $product = Product::factory()->create([
+            'name' => 'test old product',
+            'description' => 'This is old product',
+            'price' => 1000,
+            'stock' => 100,
+            'max_quantity' => 10,
+            'is_published' => 1,
+            'published_at' => now()
+        ]);
+
+        $updateData = [
+            'name' => 'test update product',
+            'description' => 'This is update product',
+            'price' => 2000,
+            'stock' => 200,
+            'max_quantity' => 20,
+            'is_published' => 0,
+            'published_at' => null
+        ];
+
+        $service = new ProductService();
+        $result = $service->updateProduct($product, $updateData);
+
+        $this->assertTrue($result);
+
+        $this->assertDatabaseHas('products', [
+            'id' => $product->id,
+            'name' => 'test update product',
+            'description' => 'This is update product',
+            'price' => 2000,
+            'stock' => 200,
+            'max_quantity' => 20,
+            'is_published' => 0,
+            'published_at' => null,
+        ]);
+    }
 }
