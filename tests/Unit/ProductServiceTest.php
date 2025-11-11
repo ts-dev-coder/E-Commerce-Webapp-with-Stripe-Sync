e56db11ea6cc1f6de63bca838355cb55827402d5
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-11 20:24:43 +0900

    Add product delete method and test code

--- tests/Unit/ProductServiceTest.php
+++ tests/Unit/ProductServiceTest.php
@@ -97,4 +97,21 @@ class ProductServiceTest extends TestCase
         $this->expectException(\Illuminate\Database\QueryException::class);
         $service->updateProduct($product, $invalidUpdateData);
     }
+
+    public function test_product_can_be_deleted()
+    {
+        $product = Product::factory()->create([
+            'name' => 'test name',
+            'description' => 'test description',
+            'price' => 1000,
+            'stock' => 100,
+            'max_quantity' => 10,
+            'is_published' => false,
+            'published_at' => now()
+        ]);
+
+        $service = new ProductService();
+
+        $this->assertTrue($service->deleteProduct($product));
+    }
 }
