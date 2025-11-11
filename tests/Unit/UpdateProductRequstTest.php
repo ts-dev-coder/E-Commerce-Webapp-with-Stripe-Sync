9c9aca3e3521837bf8cec98280b110d67f2784b6
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-11 12:06:39 +0900

    test code : add test code for required columns

--- tests/Unit/UpdateProductRequstTest.php
+++ tests/Unit/UpdateProductRequstTest.php
@@ -24,4 +24,72 @@ class UpdateProductRequstTest extends TestCase
 
         $this->assertTrue($validator->passes());
     }
+
+    public function test_name_is_required()
+    {
+        $request = new UpdateProductRequest();
+
+        $validData = [
+            'description' => 'hello world',
+            'price' => 1000,
+            'stock' => 10,
+            'is_published' => true,
+        ];
+
+        $validator = Validator::make($validData, $request->rules());
+
+        $this->assertTrue($validator->fails());
+        $this->assertArrayHasKey('name', $validator->errors()->toArray());
+    }
+
+    public function test_price_is_required()
+    {
+        $request = new UpdateProductRequest();
+
+        $validData = [
+            'name' => 'test name',
+            'description' => 'hello world',
+            'stock' => 10,
+            'is_published' => true,
+        ];
+
+        $validator = Validator::make($validData, $request->rules());
+
+        $this->assertTrue($validator->fails());
+        $this->assertArrayHasKey('price', $validator->errors()->toArray());
+    }
+
+    public function test_stock_is_required()
+    {
+        $request = new UpdateProductRequest();
+
+        $validData = [
+            'name' => 'test name',
+            'description' => 'hello world',
+            'price' => 1000,
+            'is_published' => true,
+        ];
+
+        $validator = Validator::make($validData, $request->rules());
+
+        $this->assertTrue($validator->fails());
+        $this->assertArrayHasKey('stock', $validator->errors()->toArray());
+    }
+
+    public function test_is_published_is_required()
+    {
+        $request = new UpdateProductRequest();
+
+        $validData = [
+            'name' => 'test name',
+            'description' => 'hello world',
+            'price'=> 1000,
+            'stock' => 10,
+        ];
+
+        $validator = Validator::make($validData, $request->rules());
+
+        $this->assertTrue($validator->fails());
+        $this->assertArrayHasKey('is_published', $validator->errors()->toArray());
+    }
 }
