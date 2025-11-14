cc3928893e88ad9a63caa956539575ae465804f2
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-14 16:28:45 +0900

    Add setUp method in the SalesRepositoryTest class

--- tests/Unit/SalesRepositoryTest.php
+++ tests/Unit/SalesRepositoryTest.php
@@ -2,9 +2,17 @@
 
 namespace Tests\Unit;
 
+use App\Repositories\Admin\SalesRepository;
 use Tests\TestCase;
 
 class SalesRepositoryTest extends TestCase
 {
+    protected SalesRepository $respository;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->respository = new SalesRepository();
+    }
 }
