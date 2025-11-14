ffa914453aa27069180dda2d191eb59c8a34567e
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-14 16:28:11 +0900

    Add SalesRepository class

---
+++ app/Repositories/Admin/SalesRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Admin;
+
+class SalesRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
