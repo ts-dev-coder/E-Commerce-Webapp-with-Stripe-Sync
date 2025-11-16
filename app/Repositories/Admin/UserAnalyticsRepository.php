70d4bfbdab03061891f4fe166e47eae27156b67e
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-16 12:10:02 +0900

    Add UserAnalyticsRepository class file

---
+++ app/Repositories/Admin/UserAnalyticsRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Admin;
+
+class UserAnalyticsRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
