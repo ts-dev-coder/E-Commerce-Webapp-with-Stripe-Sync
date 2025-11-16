43fda780baf373be2bbec8a5d792dcab85e3feb4
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-16 12:10:23 +0900

    Add UserAnalyticsService class file

---
+++ app/Services/Admin/UserAnalyticsService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Admin;
+
+class UserAnalyticsService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
