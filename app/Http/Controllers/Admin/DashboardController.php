787b83fd47934d4b34585e0275fdc49f62e563c8
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-16 14:21:35 +0900

    Add data of sales and user trend

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-
+use App\Services\Admin\SalesAnalyticsService;
+use App\Services\Admin\UserAnalyticsService;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    public function __invoke()
+    public function __invoke(SalesAnalyticsService $salesAnalyticsService, UserAnalyticsService $userAnalyticsService)
     {
-        return Inertia::render('admin/dashboard');
+        $sales = $salesAnalyticsService->getDailySales(365);
+        $users = $userAnalyticsService->getDailyUsersCountTrend(365);
+
+        return Inertia::render('admin/dashboard', [
+            'salesTrend' => $sales,
+            'userTrend' => $users
+        ]);
     }
 }
