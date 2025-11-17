f04d44a5d7fdf7b887552697b7a9851fb0754e74
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-17 21:42:14 +0900

    Add AdminMiddleware class

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!$request->user()) {
+            redirect(route('login'));
+        }
+
+        if(!$request->user()->isAdmin()) {
+            abort(403, 'You are not authorized to access this page.');
+        }
+
+        return $next($request);
+    }
+}
