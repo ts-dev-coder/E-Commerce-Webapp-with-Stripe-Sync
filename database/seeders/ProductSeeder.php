409a44e5441bf5e6476071fffb6da318f680ae4c
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-11-20 19:59:50 +0900

    Change generate product count 30 to 100

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -8,11 +8,13 @@ use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Product::factory()->count(30)->create();
+        Product::factory()->count(100)->create();
     }
 }
