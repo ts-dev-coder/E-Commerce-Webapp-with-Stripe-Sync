dfaa700bbbbf461b57306c0349282d84b1affea7
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-12-03 16:11:05 +0900

    refactor: remove outdated test for retrieving latest products

--- tests/Unit/ProductServiceTest.php
+++ tests/Unit/ProductServiceTest.php
@@ -13,32 +13,6 @@ class ProductServiceTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_it_returns_the_latest_10_products()
-    {
-        Product::factory()
-            ->count(15)
-            ->sequence(fn ($sequence) => [
-                'created_at' => now()->subMinutes($sequence->index),
-            ])
-            ->create();
-        
-        
-        $service = new ProductService();
-        $products = $service->retrieveLatestProducts();
-
-        // Productsの配列の数が10個であること
-        $this->assertCount(10, $products);
-        
-        // 配列の中身が最新順になっていること
-        $this->assertTrue(
-            $products->first()->created_at->gt($products->last()->created_at),
-            'Products are not ordered by latest.'
-        );
-
-        // 配列の中身がProduct instanceであること
-        $this->assertInstanceOf(Product::class, $products->first());
-    }
-
     public function test_product_can_be_updated()
     {
         $product = Product::factory()->create([
