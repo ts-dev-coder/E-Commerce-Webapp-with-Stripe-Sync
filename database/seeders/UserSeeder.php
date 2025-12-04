99d5c85d59ded78679ddfb4bd2d65a37b64752eb
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-12-05 08:57:00 +0900

    refactor: update how to generate the created_at column data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,13 +22,26 @@ class UserSeeder extends Seeder
             'email' => 'admin@example.com',
             'role' => 'admin',
         ]);
-
+        
+        /**
+         * Seeder fileの実行日より一年の間でユーザー生成を行いかつ、実際のアプリのように
+         * 日付が新しくなればなるほどユーザーの登録数が増えるような仕様
+         */
         User::factory(100)->create([
             'created_at' => function () {
+                $maxDays = 365;
+                // 調整値: lambdaが大きいほど最近にユーザーの作成日が集中する
+                $lambda = 1 / 60; 
+
+                $daysAgo = intval(-log(1 - mt_rand() / mt_getrandmax()) / $lambda);
+
+                // 上限を365日以内にする
+                $daysAgo = min($daysAgo, $maxDays);
+
                 return Carbon::now()
-                ->subDays(rand(0, 365))
-                ->subHours(rand(0, 23))
-                ->subMinutes(rand(0, 59));
+                    ->subDays($daysAgo)
+                    ->subHours(rand(0, 23))
+                    ->subMinutes(rand(0, 59));
             },
             'updated_at' => fn($attr) => $attr['created_at'],
         ]);
