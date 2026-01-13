7fafb118b16201bae20021abd2f2e5a95bc279f3
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-01-13 11:43:56 +0900

    test: add unit test to verify first address is set as default

---
+++ tests/Unit/Models/UserAddressTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserAddressTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_first_address_becomes_default()
+    {
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
+
+        $result = $user->addAddress([
+            'recipient_name' => '山田 太郎',
+            'postal_code'    => '1000001',
+            'prefecture'     => '東京都',
+            'city'           => '千代田区',
+            'street'         => '千代田1-1',
+            'building'       => null,
+            'phone_number'   => '09012345678',
+        ]);
+
+        $this->assertTrue($result->is_default);
+    }    
+}
