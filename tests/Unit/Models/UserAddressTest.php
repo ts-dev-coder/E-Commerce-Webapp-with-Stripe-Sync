f4d1d1d8b7fdf513f595827b50695499fe78a80b
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-01-14 12:32:14 +0900

    Let's pause the work for now

--- tests/Unit/Models/UserAddressTest.php
+++ tests/Unit/Models/UserAddressTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\Address;
 use App\Models\User;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -28,4 +29,31 @@ class UserAddressTest extends TestCase
 
         $this->assertTrue($address->is_default);
     }    
+
+    public function test_it_unsets_existing_default_when_new_default_is_set()
+    {
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
+
+        Address::factory()->create([
+            'user_id' => $user->id,
+            'is_default' => true,
+        ]);
+
+        $newAddress = $user->addAddress([
+            'recipient_name' => '山田 太郎',
+            'postal_code'    => '1000001',
+            'prefecture'     => '東京都',
+            'city'           => '千代田区',
+            'street'         => '千代田1-1',
+            'building'       => null,
+            'phone_number'   => '09012345678',
+            'is_default'     => true,
+            'user_id'        => $user->id,
+        ]);
+        
+        // $result = $user->defaultAddress;
+
+        // $this->assertTrue($newAddress->is($result));
+    }
 }
